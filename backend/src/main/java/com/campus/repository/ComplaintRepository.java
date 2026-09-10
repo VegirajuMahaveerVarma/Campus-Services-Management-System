a4f3389611dc3c3641aca4aaabdb3a239765f700
VEGIRAJU MAHAVEER VARMA <86178275+VegirajuMahaveerVarma@users.noreply.github.com>
2026-09-10 20:49:00 +0530

    Add complaint repository

---
+++ backend/src/main/java/com/campus/repository/ComplaintRepository.java
@@ -0,0 +1,5 @@
+package com.campus.repository;
+import com.campus.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+public interface ComplaintRepository extends JpaRepository<Complaint,Long>{ List<Complaint> findByStudentUserEmailOrderByCreatedAtDesc(String email); List<Complaint> findAllByOrderByCreatedAtDesc(); List<Complaint> findByStatusOrderByCreatedAtDesc(ComplaintStatus status); }
