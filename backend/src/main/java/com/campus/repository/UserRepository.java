128aefd8aa195c0564a81376bc9e2a2a578e186a
VEGIRAJU MAHAVEER VARMA <86178275+VegirajuMahaveerVarma@users.noreply.github.com>
2026-09-10 20:48:53 +0530

    Add user repository

---
+++ backend/src/main/java/com/campus/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.campus.repository;
+import com.campus.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User,Long>{ Optional<User> findByEmail(String email); }
