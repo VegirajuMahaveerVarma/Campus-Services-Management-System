cf5cf1aae39113a94396e85c18c6fc5c576a4f4c
VEGIRAJU MAHAVEER VARMA <86178275+VegirajuMahaveerVarma@users.noreply.github.com>
2026-09-10 20:47:26 +0530

    Add initial MySQL database schema

---
+++ database/schema.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS campus_services;
+USE campus_services;
+
+-- Core schema will be expanded with JPA entities and migrations.
+-- Planned tables: users, students, complaints, events, notices.
