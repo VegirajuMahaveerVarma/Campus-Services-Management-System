52295e50ada8516daa1b660461f912c4692d7d1e
VEGIRAJU MAHAVEER VARMA <86178275+VegirajuMahaveerVarma@users.noreply.github.com>
2026-09-11 11:16:22 +0530

    feat: add upcoming event analytics query

--- backend/src/main/java/com/campus/repository/EventRepository.java
+++ backend/src/main/java/com/campus/repository/EventRepository.java
@@ -1,5 +1,9 @@
 package com.campus.repository;
 import com.campus.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
 import java.util.List;
-public interface EventRepository extends JpaRepository<Event,Long>{ List<Event> findAllByOrderByEventDateAsc(); }
+public interface EventRepository extends JpaRepository<Event,Long>{
+ List<Event> findAllByOrderByEventDateAsc();
+ List<Event> findByEventDateAfter(LocalDateTime date);
+}
