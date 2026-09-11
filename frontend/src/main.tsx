7d1857efd81c70434a4caf5dd5ad963f7471acc3
VEGIRAJU MAHAVEER VARMA <86178275+VegirajuMahaveerVarma@users.noreply.github.com>
2026-09-11 14:25:42 +0530

    feat: enable event registration UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Phase2App';
 import './styles.css';
+import './eventRegistrationEnhancer';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
