f3f0d954b59e0c0a615eec7d926db0c7608b2118
VEGIRAJU MAHAVEER VARMA <86178275+VegirajuMahaveerVarma@users.noreply.github.com>
2026-09-11 14:46:38 +0530

    feat: mount holiday calendar and student services

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './Phase2App';
 import './styles.css';
 import './eventRegistrationEnhancer';
+import './noticeEnhancer';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
